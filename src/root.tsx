0ac0d7a9077013cfbbe4be3189bd38e4e858203a
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-02-06 16:26:04 +0100

    258

--- src/root.tsx
+++ src/root.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import DispatcherExample from './apps/dispatcherExample.app';
+import { todoProvider as TDP } from './context/todoContext';
+import { reduxProvider as RP } from './context/reduxContext';
 import Redux from './apps/redux.app';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min';
-import { todoProvider as TDP } from './context/todoContext';
+
+
 
 const renderApp = {
   dispatcherExample:() => ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode><TDP><DispatcherExample/></TDP></React.StrictMode>),
-  redux:() => ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<React.StrictMode><Redux/></React.StrictMode>)
+  redux:() => ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <RP><Redux/></RP>
+    </React.StrictMode>
+  )
 };
 
 renderApp.redux()
